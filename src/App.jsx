e8a3cd17627a2af7f24ad69b2730781f42528bbe
LiamSG02 <liamswansson@gmail.com>
2022-05-09 16:55:16 +0200

    added images and implemented navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 
-
-
 export const App = () => {
   return (
     <>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,2 +1,17 @@
 import React from "react";
 import './navbar.css'
+
+
+export const Navbar = () => {
+    return (
+
+    <nav class="nav collapsible">
+        <a class="nav__logo" href="/"><img src="images/Malta.jpg" alt=""/></a>
+    </nav>
+    
+    )
+  }
+
+
+
+export default Navbar
